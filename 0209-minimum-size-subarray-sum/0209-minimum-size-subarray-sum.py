1993c0d05d469f314a8abca99658cf1ec7c55038
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2024-09-17 13:15:10 -0400

    Time: 379 ms (8.08%), Space: 21.8 MB (96.52%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -6,19 +6,74 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        window_sum = 0
-        min_len = float('inf')
         left = 0
+        min_len = float('inf')
+        window_sum = 0
         
-        
-        for right in range(n):
+        for right in range (n):
             window_sum += nums[right]
-            # If the sum exceeds the target, then we need to delete the left element while keeping the length minimum.
             while window_sum >= target:
-                min_len = min(min_len , right - left + 1)
-                window_sum -= nums[left]
+                #left = left +1
+                min_len = min(min_len, right-left+1)
+                window_sum = window_sum - nums[left]
                 left += 1
+        
+        if min_len!= float('inf'):
+            return min_len
+        else:
+            return 0
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(nums)
+#         window_sum = 0
+#         min_len = float('inf')
+#         left = 0
+        
+        
+#         for right in range(n):
+#             window_sum += nums[right]
+#             # If the sum exceeds the target, then we need to delete the left element while keeping the length minimum.
+#             while window_sum >= target:
+#                 min_len = min(min_len , right - left + 1)
+#                 window_sum -= nums[left]
+#                 left += 1
                 
-        return min_len if min_len!= float('inf') else 0
+#         return min_len if min_len!= float('inf') else 0
             
             
