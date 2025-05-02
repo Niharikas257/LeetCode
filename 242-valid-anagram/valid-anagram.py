8f0e344e37e2d73c9400a6e021347cc50a46bd57
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-05-02 18:16:49 -0400

    Time: 14 ms (46.33%) | Memory: 17.7 MB (86.30%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        table = {}
+        if len(s) != len(t):
+            return False
+
+        for i in s:
+            if i in table:
+                table[i] += 1
+            else:
+                table[i] = 1
+
+        for j in t:
+                if j not in table or table[j] == 0 :
+                    return False
+                table[j] -= 1
+        return True
+
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+#         table = {}
+#         for ch in s:
+#             table[ch] = table.get(ch, 0) + 1
+#         for ch in t:
+#             if ch not in table or table[ch] == 0:
+#                 return False
+#             table[ch] -= 1
+
+#         return True
+
+        
