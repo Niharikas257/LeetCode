9f720ad44ab966e6e9ed1f45f6a10f0737fc9dbd
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-05-02 16:51:32 -0400

    Time: 19 ms (23.05%) | Memory: 34.8 MB (17.53%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,13 +1,22 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        table = {}
-        for i in nums:
-            if i not in table:
-                table[i] = 0
+        # table = {}
+        # for i in nums:
+        #     if i not in table:
+        #         table[i] = 0
+        #     else:
+        #         return True
+        # return False
+        tracker = {}
+        
+        for item in nums:
+            if item not in tracker:
+                tracker[item] = 0
             else:
                 return True
+        
         return False
-                
+            
        
             
             
