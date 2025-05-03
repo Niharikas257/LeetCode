de515e9a1db6879f593a283683250b94b176701f
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-05-03 17:05:41 -0400

    Time: 0 ms (100.00%) | Memory: 13.1 MB (61.53%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # hash_map={}
+        
+        # for index,value in enumerate(nums):
+        #     remainder=target-value
+            
+        #     if remainder in hash_map:
+        #         return [hash_map[remainder], index]
+            
+        #     hash_map[value]=index
+            
+        
+        table = {}
+
+        for index, value in enumerate(nums):
+            remainder = target - value
+
+            if remainder in table:
+                return[table[remainder], index]
+            
+            table[value] = index
