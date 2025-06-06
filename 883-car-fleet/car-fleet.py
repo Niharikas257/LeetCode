4213e539528f2fccf61bbf3e7fe4fdb87c4e446f
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-06-06 11:51:49 -0400

    Time: 140 ms (83.85%) | Memory: 37.6 MB (66.71%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,12 +1,27 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [(p,s) for p, s in zip(position,speed)]
-        stack = []
-        # res = []
+        # pair = [(p,s) for p, s in zip(position,speed)]
+        # stack = []
+        # # res = []
 
-        for p,s in sorted(pair)[::-1]:
-            stack.append((target-p)/s)
-            if len(stack)>=2 and stack[-1]<=stack[-2]:
-                stack.pop()
-        return len(stack)
+        # for p,s in sorted(pair)[::-1]:
+        #     stack.append((target-p)/s)
+        #     if len(stack)>=2 and stack[-1]<=stack[-2]:
+        #         stack.pop()
+        # return len(stack)
         
+        pair = [(p,s) for p,s in zip(position,speed)]
+        pair.sort(reverse=True)
+        prevTime = (target-pair[0][0])/pair[0][1]
+        fleet = 1
+
+        for i in range(1, len(pair)):
+            currcar = pair[i]
+            currTime = (target-currcar[0])/currcar[1]
+            if currTime > prevTime:
+                fleet += 1
+                prevTime = currTime
+        return fleet
+
+
+            
