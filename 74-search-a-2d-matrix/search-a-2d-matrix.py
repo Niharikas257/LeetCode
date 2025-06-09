876b5649fe46ea615d663532dd8e2d97b2b9ed65
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-06-09 07:35:05 -0400

    Time: 3 ms (3.78%) | Memory: 18.4 MB (26.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,35 +1,39 @@
-# class Solution:
-#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-#         ROWS = len(matrix)
-#         COLS = len(matrix[0])
-
-#         l = 0
-#         r = ROWS * COLS -1
-
-#         while l<=r:
-#             m = l + (r-l)//2
-#             row = m //COLS
-#             col = m % COLS
-#             if target > matrix[row][col]:
-#                 l = m + 1
-#             elif target < matrix[row][col]:
-#                 r = m-1
-#             else:
-#                 return True
-#         return False
-            
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
+        if not matrix or not matrix[0]:
+            return False
 
-        l, r = 0, ROWS * COLS - 1
-        while l <= r:
-            m = l + (r - l) // 2
-            row, col = m // COLS, m % COLS
+        
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        l = 0
+        r = ROWS * COLS -1
+
+        while l<=r:
+            m = l + (r-l)//2
+            row = m //COLS
+            col = m % COLS
             if target > matrix[row][col]:
                 l = m + 1
             elif target < matrix[row][col]:
-                r = m - 1
+                r = m-1
             else:
                 return True
         return False
+            
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         ROWS, COLS = len(matrix), len(matrix[0])
+
+#         l, r = 0, ROWS * COLS - 1
+#         while l <= r:
+#             m = l + (r - l) // 2
+#             row, col = m // COLS, m % COLS
+#             if target > matrix[row][col]:
+#                 l = m + 1
+#             elif target < matrix[row][col]:
+#                 r = m - 1
+#             else:
+#                 return True
+#         return False
