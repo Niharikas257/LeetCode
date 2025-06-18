7552853d58431a9be8fe145ccc47c8d47deca5e0
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-06-18 08:26:37 -0400

    Time: 4 ms (96.62%) | Memory: 20.4 MB (36.37%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         res = ListNode(0)
+#         cur = res
+
+#         while True:
+#             minNode = -1
+#             for i in range(len(lists)):
+#                 if not lists[i]:
+#                     continue
+#                 if minNode == -1 or lists[minNode].val > lists[i].val:
+#                     minNode = i
+
+#             if minNode == -1:
+#                 break
+#             cur.next = lists[minNode]
+#             lists[minNode] = lists[minNode].next
+#             cur = cur.next
+#         return res.next        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        nodes = []
+        for lst in lists:
+            while lst:
+                nodes.append(lst.val)
+                lst = lst.next
+        nodes.sort()
+
+        res = ListNode(0)
+        cur = res
+        for node in nodes:
+            cur.next = ListNode(node)
+            cur = cur.next
+        return res.next
