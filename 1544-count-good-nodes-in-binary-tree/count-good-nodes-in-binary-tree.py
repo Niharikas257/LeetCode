d47c79e4c996e4c8ce62760eb4fcde8e27e099d6
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-06-30 10:44:01 -0400

    Time: 173 ms (5.26%) | Memory: 32.2 MB (63.65%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,25 +6,41 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        stack = [(root, root.val)]
-        good = []
-        while stack:
-            node, max_so_far = stack.pop()
+        # if not root:
+        #     return 0
+        # stack = [(root, root.val)]
+        # good = []
+        # while stack:
+        #     node, max_so_far = stack.pop()
 
-            if node.val >= max_so_far:
-                good.append(node.val)
-            new_max = max(max_so_far, node.val)
+        #     if node.val >= max_so_far:
+        #         good.append(node.val)
+        #     new_max = max(max_so_far, node.val)
 
-            if node.right:
-                stack.append((node.right, new_max))
+        #     if node.right:
+        #         stack.append((node.right, new_max))
                 
 
-            if node.left:
-                stack.append((node.left, new_max))
+        #     if node.left:
+        #         stack.append((node.left, new_max))
                 
             
-        return len(good)
+        # return len(good)
+
+
+        def dfs(node, max_val):
+            if not node:
+                return 0
+            res = 1 if node.val >= max_val else 0
+
+            max_val = max(max_val, node.val)
+            res += dfs(node.right, max_val)
+            res += dfs(node.left, max_val)
+            return res
+        
+        return dfs(root, root.val)
+        
+
+
 
 
