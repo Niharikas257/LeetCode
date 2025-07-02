7affe6f248c3b8c037cdd840387bdd717b9d66df
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-07-02 16:42:30 -0400

    Time: 1 ms (14.60%) | Memory: 17.9 MB (32.17%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -2,18 +2,29 @@ import heapq
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        # Simulate a max heap by negating all the values
+        # # Simulate a max heap by negating all the values
+        # stones = [-s for s in stones]
+        # heapq.heapify(stones)
+
+        # while len(stones) > 1:
+        #     # Get two heaviest stones
+        #     first = -heapq.heappop(stones)
+        #     second = -heapq.heappop(stones)
+            
+        #     if first != second:
+        #         # Push the difference back into heap
+        #         heapq.heappush(stones, -(first - second))
+
+        # # Return the last remaining stone or 0
+        # return -stones[0] if stones else 0
         stones = [-s for s in stones]
         heapq.heapify(stones)
 
-        while len(stones) > 1:
-            # Get two heaviest stones
-            first = -heapq.heappop(stones)
-            second = -heapq.heappop(stones)
-            
-            if first != second:
-                # Push the difference back into heap
-                heapq.heappush(stones, -(first - second))
+        while len(stones)>1:
+            x = -heapq.heappop(stones)
+            y = -heapq.heappop(stones)
 
-        # Return the last remaining stone or 0
+            if x != y:
+                heapq.heappush(stones, (y-x))
+
         return -stones[0] if stones else 0
