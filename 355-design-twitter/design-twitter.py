40cf28596663f47795cd2862c9d93b2a45ed6adb
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-07-09 13:11:29 -0400

    Time: 22 ms (25.98%) | Memory: 27.8 MB (18.07%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -10,24 +10,43 @@ class Twitter:
         self.count -= 1        
 
     def getNewsFeed(self, userId: int) -> List[int]:
+        # res = []
+        # minheap = []
+
+        # self.followmap[userId].add(userId)
+        # for followeeId in self.followmap[userId]:
+        #     if followeeId in self.tweetmap:
+        #         index = len(self.tweetmap[followeeId])-1
+        #         count, tweetId = self.tweetmap[followeeId][index]
+        #         heapq.heappush(minheap, [count, tweetId, followeeId, index-1])
+
+        # while minheap and len(res) < 10:
+        #     count, tweetId, followeeId, index = heapq.heappop(minheap)
+        #     res.append(tweetId)
+        #     if index >= 0:
+        #         count, tweetId = self.tweetmap[followeeId][index]
+        #         heapq.heappush(minheap, [count, tweetId, followeeId, index - 1])
+        # return res
         res = []
         minheap = []
 
         self.followmap[userId].add(userId)
-        for followeeId in self.followmap[userId]:
+        for followeeId in self.followmap[userId]: # for the followee in the user's follow list, if that followee ID is in tweetmap, means that person has tweeted in the past
             if followeeId in self.tweetmap:
                 index = len(self.tweetmap[followeeId])-1
-                count, tweetId = self.tweetmap[followeeId][index]
-                heapq.heappush(minheap, [count, tweetId, followeeId, index-1])
-
-        while minheap and len(res) < 10:
+                count, tweetId = self.tweetmap[followeeId][index] # we want the count and tweetID of the followee's tweet from the tweetmap at a particular index inside the particular followee tweets list.
+                heapq.heappush(minheap, [count, tweetId, followeeId, index-1]) # index -1  becuase we are pushing the 2nd latest tweet of the followee.
+        heapq.heapify(minheap)
+        while minheap and len(res) < 10: 
+            # until we have elements in the min heap and until we not have 10 tweets for the newsfeed, we will keep on repeating the loop.
             count, tweetId, followeeId, index = heapq.heappop(minheap)
             res.append(tweetId)
-            if index >= 0:
+            if index >= 0: # while we still have the tweets from a particular person/followee
                 count, tweetId = self.tweetmap[followeeId][index]
-                heapq.heappush(minheap, [count, tweetId, followeeId, index - 1])
+                heapq.heappush(minheap, [count, tweetId, followeeId, index-1])
         return res
-        
+             
+
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followmap[followerId].add(followeeId)
