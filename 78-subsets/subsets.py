723f73c78902ae7cb8bdef10e47cf8a6993ec329
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-07-11 14:24:15 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.90%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,15 +4,12 @@ class Solution:
         subset = []
 
         def backtrack(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+            if i>=len(nums):
+                res.append(subset.copy()) # after the end of every loop, the subset is getting popped and if we are not using copy then every line of code is directing to the same subset from which we are popping at the end of the loop, which is empty.So, the code will run the required number of times, but it will always append empty subset/ popped subset.
                 return
-
             subset.append(nums[i])
             backtrack(i+1)
             subset.pop()
             backtrack(i+1)
         backtrack(0)
         return res
-
-        
