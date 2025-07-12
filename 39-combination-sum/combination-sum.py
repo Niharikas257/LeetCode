07b0532e430e8fb515de5dde745ee18f76e41a2b
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-07-12 09:36:31 -0400

    Time: 11 ms (58.02%) | Memory: 17.8 MB (61.70%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,17 +1,31 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # res = []
+        # def dfs(i, subset, total):
+        #     if total == target:
+        #         res.append(subset.copy())
+        #         return
+        #     if i >= len(candidates) or total > target:
+        #         return
+        #     subset.append(candidates[i]) # This loop will keep on adding the same number again and again until either the number becomes greater than target or equal to it.
+        #     dfs(i, subset,total+candidates[i])
+        #     subset.pop()
+        #     dfs(i+1, subset, total)
+        # dfs(0,[],0)
+        # return res
         res = []
-        subset = [] # this will contain the elements which adds up to the target.
-        def dfs(i,subset,total):
-            if total == target:
+        subset= []
+        def dfs(i, subset, total):
+            if total==target:
                 res.append(subset.copy())
                 return
-            if i>= len(candidates) or total>target:
+            if i >= len(candidates) or total > target:
                 return
+
             subset.append(candidates[i])
             dfs(i, subset, total+candidates[i])
             subset.pop()
             dfs(i+1, subset, total)
-        dfs(0, [], 0)
+        dfs(0, [],0)
         return res
-        
+
