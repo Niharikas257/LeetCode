eea04dcc0fbc905713cc0a18e155a7c004c3e1c7
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2025-07-13 14:43:11 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.46%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,25 +1,38 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # res = []
+        # subset = []
+        # nums.sort()
+        # seen =[]
+        # def backtrack(i):
+        #     if i >= len(nums):
+        #         t = tuple(subset)
+        #         if t not in seen:
+
+        #             res.append(subset.copy())
+        #             seen.append(t)
+        #         return
+            
+        #     subset.append(nums[i])
+        #     backtrack(i+1)
+        #     subset.pop()
+        #     # while i+1 < len(nums) and nums[i] == nums[i+1]:
+        #     #     i+=1 # skip the element is the elements are same
+        #     backtrack(i+1)    
+            
+        # backtrack(0)
+        # return res
         res = []
         subset = []
         nums.sort()
-        seen =[]
-        def backtrack(i):
-            if i >= len(nums):
-                t = tuple(subset)
-                if t not in seen:
-
-                    res.append(subset.copy())
-                    seen.append(t)
-                return
-            
-            subset.append(nums[i])
-            backtrack(i+1)
-            subset.pop()
-            # while i+1 < len(nums) and nums[i] == nums[i+1]:
-            #     i+=1 # skip the element is the elements are same
-            backtrack(i+1)    
-            
+        def backtrack(start):
+            res.append(subset.copy())
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                subset.append(nums[i])
+                backtrack(i+1)
+                subset.pop()
         backtrack(0)
         return res
         
