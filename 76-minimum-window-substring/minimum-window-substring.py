225ca7cb96c3bbb787a0e343b2ca4449d28926fd
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-08-28 11:19:53 -0400

    Time: 63 ms (80.39%) | Memory: 18.2 MB (54.55%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,37 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        if not s or not t:
             return ""
-        targetcount = {}
-        window = {}
-
-        for c in t:
-            targetcount[c] = 1 + targetcount.get(c, 0)
         
-        need = len(targetcount)
-        have = 0
-        l = 0
-        res = [-1,-1]
-        resLen=float("infinity")
-        for r in range(len(s)):
-            c = s[r]
+        countT = {}
+        for c in t:
+            countT[c] = 1 + countT.get(c,0)
+
+        left = 0
+        window = {}
+        res= [-1,-1]
+        resLen = float("infinity")
+        what_we_have = 0
+        what_we_need = len(countT)
+
+        for right in range(len(s)):
+            c = s[right]
             window[c] = 1 + window.get(c,0)
 
-            if c in targetcount and targetcount[c] == window[c]:
-                have += 1
-            
-            while have == need:
-                if (r-l+1) < resLen:
-                    res = [l,r]
-                    resLen = r-l+1
-                window[s[l]] -= 1
-                if s[l] in targetcount and window[s[l]]<targetcount[s[l]]:
-                    have -= 1
-                l+=1
+            if c in countT and window[c] == countT[c]:
+                what_we_have += 1
 
-        l,r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
-        # return s[l : r + 1] if resLen != float("infinity") else ""
+            while what_we_have == what_we_need:
+                if (right-left+1)< resLen:
+                    res = [left,right]
+                    resLen = (right-left+1)
+
+                window[s[left]] -= 1
+                if s[left] in t and window[s[left]] < countT[s[left]]:
+                    what_we_have -= 1
+                left += 1
+        left,right = res
+        return s[left:right+1] if resLen!=float("infinity") else ""
+
+
+        
+
 
 
 
