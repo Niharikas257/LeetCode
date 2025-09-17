e085be3f6d524276b951a6b8b8962d70f3e69fec
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-09-17 18:19:52 -0400

    Time: 19 ms (86.28%) | Memory: 23.3 MB (80.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,7 +4,7 @@ class Solution:
         prefix = 1
         answer = [1]*n
         for i in range(n):
-            answer[i] *= prefix
+            answer[i] = prefix
             prefix *= nums[i]
         suffix = 1
         for i in range(n-1, -1, -1):
