1cea34234d1b7d54b76454840cb8574276a2f2cd
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-10-22 13:00:29 -0400

    Time: 3 ms (5.62%) | Memory: 17.7 MB (69.26%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,6 +1,32 @@
+# class Solution:
+#     def letterCombinations(self, digits: str) -> List[str]:
+#         res = []
+#         digitToChar = {
+#             "2": "abc",
+#             "3": "def",
+#             "4": "ghi",
+#             "5": "jkl",
+#             "6": "mno",
+#             "7": "qprs",
+#             "8": "tuv",
+#             "9": "wxyz",
+#         }
+#         def dfs(i, subset): 
+#             # i will track the digits
+#             if len(subset) == len(digits):
+#                 res.append(subset)
+#                 return
+#             for c in digitToChar[digits[i]]:
+#                 dfs(i+1, subset + c)
+#         if digits:
+#             dfs(0,"")
+#         return res
+        
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
+        if not digits:
+            return
         digitToChar = {
             "2": "abc",
             "3": "def",
@@ -11,14 +37,16 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-        def dfs(i, subset): 
-            # i will track the digits
-            if len(subset) == len(digits):
-                res.append(subset)
+        def backtrack(i, path):
+            if i == len(digits):
+                res.append("".join(path))
                 return
-            for c in digitToChar[digits[i]]:
-                dfs(i+1, subset + c)
-        if digits:
-            dfs(0,"")
+
+            possibleLetters = digitToChar[digits[i]]
+            for letter in possibleLetters:
+                path.append(letter)
+                backtrack(i+1, path)
+                path.pop()
+        backtrack(0,[])
         return res
-        
+
