11b5bc86dc1e8b974f7a41a6efb4d8ef12921f78
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-17 18:47:06 -0500

    Time: 17 ms (19.94%) | Memory: 18.6 MB (11.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,53 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        table = {}
-        if len(s) != len(t):
+        S = list(s)
+        T = list(t)
+        if len(S) != len(T):
             return False
-        
-        for i in s:
-            if i in table:
-                table[i] += 1
-            else:
-                table[i] = 1
-            
-        for j in t:
-            if j not in table or table[j] == 0:
+        S.sort()
+        T.sort()
+        # return S == T
+        # count = len(S)
+        # cnt = 0
+        for i in range(len(S)):
+            # for j in range(len(T)):
+            if S[i] != T[i]:
                 return False
-            table[j] -= 1
-        
+
         return True
-
-        # table = {}
-        # if len(s) != len(t):
-        #     return False
-
-        # for i in s:
-        #     if i in table:
-        #         table[i] += 1
-        #     else:
-        #         table[i] = 1
-
-        # for j in t:
-        #         if j not in table or table[j] == 0 :
-        #             return False
-        #         table[j] -= 1
-        # return True
-
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
-#         table = {}
-#         for ch in s:
-#             table[ch] = table.get(ch, 0) + 1
-#         for ch in t:
-#             if ch not in table or table[ch] == 0:
-#                 return False
-#             table[ch] -= 1
-
-#         return True
-
-
-
+                
         
+                    
