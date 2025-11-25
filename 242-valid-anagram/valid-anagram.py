6e3441bccf6512e947a193b9e432cac7c34b4aac
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-25 12:15:05 -0500

    Time: 11 ms (72.41%) | Memory: 17.6 MB (99.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,40 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        S = list(s)
-        T = list(t)
-        if len(S) != len(T):
+        # S = list(s)
+        # T = list(t)
+        # if len(S) != len(T):
+        #     return False
+        # S.sort()
+        # T.sort()
+        # # return S == T
+        # # count = len(S)
+        # # cnt = 0
+        # for i in range(len(S)):
+        #     # for j in range(len(T)):
+        #     if S[i] != T[i]:
+        #         return False
+
+        # return True
+
+###########
+
+        if len(s) != len(t):
             return False
-        S.sort()
-        T.sort()
-        # return S == T
-        # count = len(S)
-        # cnt = 0
-        for i in range(len(S)):
-            # for j in range(len(T)):
-            if S[i] != T[i]:
+        freq = {}
+        for i in s:
+            freq[i] = freq.get(i, 0) + 1
+
+        for i in t:
+            if i not in freq:
+                return False
+            freq[i] -= 1
+            if freq[i] < 0:
                 return False
 
         return True
-                
+
+        
+
+        
         
                     
