2d9ab0040f77deaf77d75aa153186fff59678deb
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-25 15:50:03 -0500

    Time: 3 ms (90.40%) | Memory: 21 MB (96.43%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,37 +1,46 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. create a hash table for the count of the numbers in the array
+        # 2. Use that hash table to create an array with the item and their frequency
+        # 3. sort the array based on the frequency
+        # 4. create a list res and append the popped values from the array while the length of the res is k.
+        # 5. return res
         count = {}
         for num in nums:
-            count[num] = 1 + count.get(num, 0)
+            count[num] = 1 + count.get(num,0)
         
-        heap = []
-        for i in count.keys():
-            heapq.heappush(heap, (count[i], i))
-            if len(heap)>k:
-                heapq.heappop(heap)
-        
+        arr = []
+        for item, cnt in count.items():
+            arr.append([cnt, item])
+        arr.sort()
+
         res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1])
+        while len(res)<k:
+            res.append(arr.pop()[1])
         return res
+
+
+##########        
+            #    1. Create a hash map
+    #    2. start entering the frequencies
+    #    3. we can use a max-heap, and we pop everytime the size of the heap increases k
+    #    4. Now, we need to save the popped values somewhere, so we will create a res array and append the popped values into it.
+
         # count = {}
         # for num in nums:
-        #     count[num] = 1+count.get(num,0)
-
+        #     count[num] = 1 + count.get(num, 0)
+        
         # heap = []
-        # for num in count.keys():
-        #     heapq.heappush(heap,(count[num], num))
+        # for i in count.keys():
+        #     heapq.heappush(heap, (count[i], i))
         #     if len(heap)>k:
         #         heapq.heappop(heap)
-
+        
         # res = []
         # for i in range(k):
         #     res.append(heapq.heappop(heap)[1])
         # return res
 
-    #    1. Create a hash map
-    #    2. start entering the frequencies
-    #    3. we can use a max-heap, and we pop everytime the size of the heap increases k
-    #    4. Now, we need to save the popped values somewhere, so we will create a res array and append the popped values into it.
+
 
         
