dfff451e0f78d2b0a5634f74869393cd90777664
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-25 10:30:45 -0500

    Time: 21 ms (22.57%) | Memory: 31.5 MB (82.97%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,20 +1,20 @@
-# class Solution:
-#     def containsDuplicate(self, nums: List[int])-> bool:
-#         res = set()
-#         for i in range(len(nums)):
-#             if nums[i] in res:
-#                 return True
-#             else:
-#                 res.add(nums[i])
-#         return False
-
 class Solution:
     def containsDuplicate(self, nums: List[int])-> bool:
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
+        res = set()
+        for i in range(len(nums)):
+            if nums[i] in res:
                 return True
+            else:
+                res.add(nums[i])
         return False
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int])-> bool:
+#         nums.sort()
+#         for i in range(len(nums)-1):
+#             if nums[i] == nums[i+1]:
+#                 return True
+#         return False
 
 
         
