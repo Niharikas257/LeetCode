37069c593a43824d53b0ce9e48e016d65be3fea5
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 09:25:50 -0500

    Time: 39 ms (96.69%) | Memory: 34.4 MB (30.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,15 +1,33 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
+        # numSet = set(nums)
+        # longest = 0
+        # for num in numSet:
+        #     if (num - 1) not in numSet:
+        #         length = 1
+        #         while (num + length) in numSet:
+        #             length += 1
+        #         longest = max(length, longest)
+        # return longest
+################
+        store = set(nums)
+        res = 0
+        for num in store:
+            if (num - 1) not in store:
+                streak = 1 # If the smaller number does not exist in the store, that means it could be the start of a strak.
+                while (num + streak) in store:
+                    streak += 1
+                res = max(res, streak)
+        return res
 
-        for num in numSet:
-            if (num - 1) not in numSet:
-                length = 1
-                while (num + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
+
+
+
+
+
+
+
+
         # res = 0
         # store = set(nums)
         # for num in nums:
@@ -20,16 +38,7 @@ class Solution:
         #         cur_element += 1
         #     res = max(res, streak)
         # return res
-        # store = set(nums)
-        # res = 0
-        # for num in nums:
-        #     streak = 0
-        #     if (num-1) not in store:
-        #         streak = 1 # If the smaller number does not exist in the store, that means it could be the start of a strak.
-        #         while (num+streak) in store:
-        #             streak += 1
-        #         res = max(res, streak)
-        # return res
+
 
 
 
