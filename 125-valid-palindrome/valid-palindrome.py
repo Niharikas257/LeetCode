eb327dfb6b77dff9329aa2e948741b7beedb6ba6
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 10:39:23 -0500

    Time: 7 ms (82.57%) | Memory: 18 MB (68.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,27 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = ""
-        for c in s:
-            if c.isalnum():
-                new += c.lower()
-        return new == new[::-1]
-    #     left = 0
-    #     right = len(s) -1
-    #     while left < right:
-    #         while left < right and not self.alphaNum(s[left]):
-    #             left += 1
-    #         while right > left and not self.alphaNum(s[right]):
-    #             right -= 1
-    #         if s[left].lower() != s[right].lower():
-    #             return False
-    #         left += 1
-    #         right -= 1
+        # newstr = ""
+        # for c in s:
+        #     if c.isalnum():
+        #         newstr += c.lower()
+            
+        # return newstr == newstr[::-1]
 
-    #     return True
-    
-    # def alphaNum(self, c):
-    #     return (ord('a')<= ord(c) <= ord('z') or
-    #             ord('A') <= ord(c) <= ord('Z') or 
-    #             ord('0') <= ord(c) <= ord('9'))
 
+#  1. create a new empty string
+#  2. start iterating over the characters and if it is alphanumeric, add it to the new empty string and convert it to lower.
+#  3. return True if the new string which is only alphanumeric and lower case matches exactly with the reversed new string.
+
+#  1. we can have 2 pointers left and right
+#  2. run the loop while left < right
+#  3. while comparing we need to convert it to the lower case and also, we need to skip the character if it is not alphanumeric
+
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1 
+            while right > left and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+            
 
