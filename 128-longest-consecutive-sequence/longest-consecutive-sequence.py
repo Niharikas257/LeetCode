469e4d8858d97383e4e3a1d8b8c36e14e90579dc
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 09:21:19 -0500

    Time: 49 ms (62.68%) | Memory: 34.4 MB (30.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,18 +1,35 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        res = 0
-        numset = set(nums)
+        numSet = set(nums)
+        longest = 0
 
-        for num in numset:
-            if num-1 not in numset:
+        for num in numSet:
+            if (num - 1) not in numSet:
                 length = 1
-                current = num
-                while (current+1) in numset:
-                    current += 1
+                while (num + length) in numSet:
                     length += 1
-                res = max(res, length)
-        return res
-            
+                longest = max(length, longest)
+        return longest
+        # res = 0
+        # store = set(nums)
+        # for num in nums:
+        #     streak = 0
+        #     cur_element = num
+        #     while cur_element in store:
+        #         streak += 1
+        #         cur_element += 1
+        #     res = max(res, streak)
+        # return res
+        # store = set(nums)
+        # res = 0
+        # for num in nums:
+        #     streak = 0
+        #     if (num-1) not in store:
+        #         streak = 1 # If the smaller number does not exist in the store, that means it could be the start of a strak.
+        #         while (num+streak) in store:
+        #             streak += 1
+        #         res = max(res, streak)
+        # return res
 
 
 
