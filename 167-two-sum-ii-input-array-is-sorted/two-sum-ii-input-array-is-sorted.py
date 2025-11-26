dab525c190b7538c8231c68034dc9065ef59d31c
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 11:14:40 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (9.03%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         seen = {}
+        for i, val in enumerate(numbers):
+            remainder = target - val
+            if remainder in seen:
+                return[seen[remainder]+1, i+1]
+            seen[val] = i
 
-        for i in range(len(numbers)):
-            comp = target - numbers[i]
-            if comp in seen:
-                return (seen[comp]+1, i+1)
-            seen[numbers[i]] = i
-
-            
-        
         
