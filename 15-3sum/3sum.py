be0dd2ac88232a0aec284d5a03a28a9dfe3a4d4d
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 14:42:28 -0500

    Time: 431 ms (92.54%) | Memory: 20.6 MB (87.19%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
-
-        for i, num in enumerate(nums):
-            if num > 0: # if the number you are talking about is positive, there are no chances that you will get a number smaller than that to make the sum zero.
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] > 0:
                 break
-            if i >0 and num == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
+            
             left = i+1
-            right= len(nums) - 1
+            right = len(nums)- 1
             while left < right:
-                three_sum = num + nums[left] + nums[right]
+                three_sum = nums[i] + nums[left] + nums[right]
                 if three_sum > 0:
-                    right -= 1
+                    right -=1
                 elif three_sum < 0:
                     left += 1
-                else: 
-                    res.append([num, nums[left], nums[right]])
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    while nums[left] == nums[left -1] and left<right:
-                        left+=1
-        
+                    while left < right and nums[left] == nums[left-1]:
+                        left +=1
+
         return res
 
+
+            
