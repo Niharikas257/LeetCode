19681460aab578a11310b4f6283c387a2e4ea99d
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 21:22:46 -0500

    Time: 386 ms (6.31%) | Memory: 18.1 MB (23.19%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,11 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
+        # left = 0
         charset = set(s)
+        res = 0
 
         for c in charset:
-            left = 0
             count = 0
+            left = 0
             for right in range(len(s)):
                 if s[right] == c:
                     count += 1
@@ -13,7 +14,8 @@ class Solution:
                     if s[left] == c:
                         count -= 1
                     left += 1
-                res = max(res, right-left+1)
+                res = max(res, (right-left+1))
         return res
 
- 
+                
+
