2c3e92dd0c4e99732157f5f925d69ed79df41f84
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-26 22:37:20 -0500

    Time: 15 ms (84.82%) | Memory: 17.7 MB (97.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,30 +1,25 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        m = len(s1)
-        n = len(s2)
-
-        if m > n:
+        if len(s1) > len(s2):
             return False
-
-        count1 = [0]*26
-        count2 = [0]*26
-
-        for i in range(m):
-            # range m because we need to find maximum of length m (length of s1)
-            count1[ord(s1[i])- ord('a')] += 1
-            count2[ord(s2[i])- ord('a')] += 1
         
-        if count1 == count2:
-            return True
-
-        for r in range(m,n):
-            count2[ord(s2[r])-ord('a')] += 1
-            count2[ord(s2[r-m])-ord('a')] -= 1
-
-            if count1 == count2:
+        need = [0] * 26
+        window = [0] * 26
+        
+        for ch in s1:
+            need[ord(ch) - ord('a')] += 1
+        
+        l = 0
+        for r in range(len(s2)):
+            window[ord(s2[r]) - ord('a')] += 1
+            
+            # shrink to keep window size == len(s1)
+            if r - l + 1 > len(s1):
+                window[ord(s2[l]) - ord('a')] -= 1
+                l += 1
+            
+            # compare frequency arrays
+            if window == need:
                 return True
         
         return False
-
-
-        
