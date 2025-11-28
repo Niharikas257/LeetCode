67c8cb87053b1b89222b1d596fc8b2db4476ac18
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-27 21:02:01 -0500

    Time: 215 ms (12.19%) | Memory: 19.2 MB (20.96%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,22 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1 # minimum speed with which koko can eat banana, it can not be zero.
-        r = max(piles) # maximum probable value for the speed
+        left = 1
+        right = max(piles)
+        res = right
 
-        res = r # speed; this is the upper bound and will always work, but we will try for lower value
-        
-        while l <= r:
-            k = (l+r)//2
-            hours = 0
-            for p in piles:
-                hours += math.ceil(p/k)
-                
-            if hours <= h:
-                res = min(res,k)
-                r = k -1
+        while left <= right:
+            k = (left+right)//2
+
+            total_time = 0
+            for pile in piles:
+                total_time += math.ceil(float(pile)/k)
+            
+            if total_time <= h:
+                res = k
+                right = k-1
             else:
-                l = k + 1
+                left = k+1
         return res
+
+
                 
 
                 
