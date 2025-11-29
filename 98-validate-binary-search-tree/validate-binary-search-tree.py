ccafe58c60b9760c26d62c5c62b018c6f4f3eeb0
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-29 13:13:57 -0500

    Time: 1 ms (46.92%) | Memory: 19.8 MB (30.34%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -4,27 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        stack = []
-        cur = root
-        # res = []
-        prev = None
-
-        while stack or cur:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            
-            cur = stack.pop()
-            if prev is not None and cur.val <= prev:
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (left < node.val < right):
                 return False
-            prev = cur.val
 
-            cur = cur.right
-        return True
+            return valid(node.left, left, node.val) and valid(
+                node.right, node.val, right
+            )
+
+        return valid(root, float("-inf"), float("inf"))
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         if not root:
+#             return True
+#         stack = []
+#         cur = root
+#         # res = []
+#         prev = None
+
+#         while stack or cur:
+#             while cur:
+#                 stack.append(cur)
+#                 cur = cur.left
+            
+#             cur = stack.pop()
+#             if prev is not None and cur.val <= prev:
+#                 return False
+#             prev = cur.val
+
+#             cur = cur.right
+#         return True
 
         # for i in range(1, len(res)):
         #     if res[i] <= res[i-1]:
