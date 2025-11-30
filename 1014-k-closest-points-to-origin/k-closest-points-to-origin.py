125a53ac18ba9de12260ab68040b6e4daf936d1a
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-30 15:03:38 -0500

    Time: 79 ms (38.99%) | Memory: 22.7 MB (58.65%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,20 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         minheap = []
-        for x, y in points:
-            dist = (x**2) + (y**2) # We don't have to find th sq root because root of a largest number is also the largest.
-            minheap.append([dist, x, y])
-
+        for (x,y) in points:
+            dist = x*x + y*y
+            heapq.heappush(minheap, (dist,x,y))
+        
         heapq.heapify(minheap)
-        res = []
 
-        while k > 0:
-            dist, x, y = heapq.heappop(minheap)
-            res.append([x, y])
-            k -= 1
-        
+        res = []
+        while len(res)< k:
+            _, x, y = heapq.heappop(minheap)
+            res.append([x,y])
         return res
-            
-        
+
 
         
