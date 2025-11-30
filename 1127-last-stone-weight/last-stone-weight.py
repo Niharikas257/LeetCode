87dc1b3590ba974bea5c1f9a9bbb4997bf5ca1ba
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-30 14:38:32 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.52%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,20 +1,11 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        if not stones:
-            return 0
         maxheap = [-stone for stone in stones]
         heapq.heapify(maxheap)
-        res = 0
 
-        while len(maxheap) >1:
+        while len(maxheap)>1:
             x = -heapq.heappop(maxheap)
-            y = -heapq.heappop(maxheap)
-            # if x == y:
-            #     continue
-                
-            if x!= y:
+            y = - heapq.heappop(maxheap)
+            if x != y:
                 heapq.heappush(maxheap, -(x-y))
-                
         return -maxheap[0] if maxheap else 0
-
-        
