07c9e09fbb91585bdae656727b70e991cd1db1c8
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-11-30 21:53:18 -0500

    Time: 141 ms (45.61%) | Memory: 19.3 MB (7.82%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,33 +1,22 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # 1. A...wait for 2T then A then wait for 2T...
-        # 2. meanwhile, we can have other tasks or if there are no other tasks, we can leave it blank
-        # 3. A _ _ A _ _ A 
-        #    1 2 3 4 5 6 7
-           
-        #    time = 0
+        q = deque()
+        freqs = Counter(tasks)
+        maxheap = [-freq for freq in freqs.values()]
+        heapq.heapify(maxheap)
+        time = 0
 
-        #    time += 1
-        # 4. we need to spread the elements with maximum frequency first and then fill up the space, 2nd frequent element
-
-            freqs = Counter(tasks)
-            q = deque() # [frequency, time]
-            time = 0
-            maxheap = [-freq for freq in freqs.values()]
-            heapq.heapify(maxheap)
-
-            while maxheap or q:
-                time += 1
-                if maxheap:
-                    freq = 1 + heapq.heappop(maxheap)
-                    if freq:
-                        q.append([freq, time+n])
-
-                    
-                if q and q[0][1] == time:
-                    heapq.heappush(maxheap, q.popleft()[0])
+        while maxheap or q:
+            time += 1
+            if maxheap:
+                freq = 1 + heapq.heappop(maxheap)
+                if freq:
+                    q.append([freq, time + n])
                 
-            return time
-
+            if q and q[0][1] == time:
+                heapq.heappush(maxheap, q.popleft()[0])
+        print(time)
+        return time
+                
 
 
