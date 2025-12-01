13745b88b984a01354236c17fb7e2c25f9ab1ced
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-01 11:42:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (64.59%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
+        # visited = set()
+        path = []
 
         def backtrack(i):
-            if i>=len(nums):
-                res.append(subset.copy()) # after the end of every loop, the subset is getting popped and if we are not using copy then every line of code is directing to the same subset from which we are popping at the end of the loop, which is empty.So, the code will run the required number of times, but it will always append empty subset/ popped subset.
-                return
-            subset.append(nums[i])
-            backtrack(i+1)
-            subset.pop()
-            backtrack(i+1)
+            res.append(path[:])
+            for i in range(i, len(nums)):
+                path.append(nums[i])
+                backtrack(i+1)
+                path.pop()
         backtrack(0)
         return res
+                
+
