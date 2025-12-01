b5456e8f14376315b96ca95defdfc9c47ad11543
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-01 12:43:07 -0500

    Time: 3 ms (97.21%) | Memory: 17.6 MB (97.86%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,31 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # res = []
-        # def dfs(i, subset, total):
-        #     if total == target:
-        #         res.append(subset.copy())
-        #         return
-        #     if i >= len(candidates) or total > target:
-        #         return
-        #     subset.append(candidates[i]) # This loop will keep on adding the same number again and again until either the number becomes greater than target or equal to it.
-        #     dfs(i, subset,total+candidates[i])
-        #     subset.pop()
-        #     dfs(i+1, subset, total)
-        # dfs(0,[],0)
-        # return res
-        res = []
-        subset= []
-        def dfs(i, subset, total):
-            if total==target:
-                res.append(subset.copy())
+        res  =[]
+        path = []
+        candidates.sort()
+        # sum = 0
+        def backtrack(i, total):
+            if total == target:
+                res.append(path.copy())
                 return
-            if i >= len(candidates) or total > target:
-                return
-
-            subset.append(candidates[i])
-            dfs(i, subset, total+candidates[i])
-            subset.pop()
-            dfs(i+1, subset, total)
-        dfs(0, [],0)
+            for j in range(i,len(candidates)):
+                if total + candidates[j] > target:
+                    break
+                path.append(candidates[j])
+                backtrack(j,total + candidates[j])
+                path.pop()
+        backtrack(0,0)
         return res
+
 
