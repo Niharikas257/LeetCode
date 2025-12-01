7a2cbc6c296b793395fa91f1ef3143eb284ed58e
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-01 13:14:37 -0500

    Time: 23 ms (12.42%) | Memory: 17.7 MB (87.12%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,21 +3,19 @@ class Solution:
         candidates.sort()
         res = []
         path = []
-        total = 0
 
         def backtrack(j, total):
             if total == target:
                 res.append(path.copy())
-                return
             for i in range(j, len(candidates)):
-                if total> target:
+                if total > target:
                     return
                 path.append(candidates[i])
-                backtrack(i, total+candidates[i])
+                backtrack(i, total + candidates[i])
                 path.pop()
-
         backtrack(0, 0)
         return res
+
 
 
 
