148004b466985d90e318083e83b4847e8c303fe7
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-01 21:15:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.70%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -3,31 +3,31 @@ class Solution:
         res = []
 
         def backtrack(start, part):
+            
             if len(part) == 4:
                 if start == len(s):
                     res.append(".".join(part))
-                return res
-            
-            remaining = len(s) - start
-            remaining_parts = 4 - len(part)
-
-            # for j in range(len(remaining)):
-            if remaining < remaining_parts or remaining > 3 * remaining_parts:
                 return 
-            for length in range(1, 4): # This for loop is for the segments
+
+            remaining_part = 4 - len(part)
+            remaining = len(s) - start
+            
+            if remaining < remaining_part or remaining > 3*remaining_part:
+                return
+            
+            for length in range(1,4):
                 end = start + length
-                if end >len(s):
+                if end > len(s):
                     break
                 if s[start] == '0' and length > 1:
                     continue
-                if int(s[start:end]) > 255:
+                
+                if int(s[start:end])>255:
                     continue
-
+                
                 part.append(s[start:end])
                 backtrack(end, part)
                 part.pop()
-        backtrack(0,[])
-        return res
-
-
         
+        backtrack(0, [])
+        return res
