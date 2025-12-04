c00afa18c74298d013cdffaec03fbdf5e0961ece
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-04 09:36:59 -0500

    Time: 7 ms (24.78%) | Memory: 17.7 MB (70.75%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,10 +3,11 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         direction = [(0,1), (0,-1), (1,0),(-1,0)]
+
         fresh = 0
         visited = [[False]*cols for _ in range(rows)]
-        q = deque()
 
+        q = deque()
 
         for row in range(rows):
             for col in range(cols):
@@ -16,19 +17,25 @@ class Solution:
                     q.append((row, col, 0))
         minutes = 0
         while q:
-            r, c, t = q.popleft()
+            r, c, t = q.popleft()   
             minutes = max(minutes, t)
-            for (dr, dc) in direction:
+
+            for (dr,dc) in direction:
                 nr = r + dr
                 nc = c + dc
 
                 if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
                     grid[nr][nc] = 2
-                    fresh -= 1
+                    fresh-=1
                     q.append((nr, nc, t+1))
 
         return minutes if fresh == 0 else -1
-                
+
+
+        
+
+
+
 
 
 
