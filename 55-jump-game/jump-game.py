3b874276c1620255f52013ed56de818144e58d2b
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-07 09:25:51 -0500

    Time: 32 ms (50.32%) | Memory: 18.6 MB (45.96%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,22 +1,23 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_val = 0
-        jump_reach = 0
-
+        reachable = 0
+        max_so_far = nums[0]
         for i in range(len(nums)):
-            if i > max_val:
+            # 1. if I am at a position in the array where I can't reach while jumping, then return False
+            # 2. Also, how will I know if I can not reach here ?
+            # 3. I need to track the maximum reach from all the positions, and if that position matches where I am then I can reach here.
+            # 4. How will I the maximum reach?
+            # 5. The maximum reach from a position is i + nums[i], because I am already at i and can jump nums[i]
+            # 6. I will call i + nums[i] = reachable because if i + nums[i] is an index (less than the len(nums)) where I can reach, then it means it is reachable by me.
+            if i > reachable:
                 return False
+            reachable = max(reachable, i + nums[i])
 
-            jump_reach = i + nums[i]
+            # 7. If I am at i which is not reachable, then I need to update the value of reachable
+            if max_so_far > reachable:
+                reachable = max_so_far
 
-            if jump_reach > max_val:
-                max_val = jump_reach
-    
-            if max_val > len(nums)-1 :
+            if reachable >= len(nums) - 1:
                 return True
-
         return True
-
-            
-
-        
+  
