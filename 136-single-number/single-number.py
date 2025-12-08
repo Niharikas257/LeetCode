450b691e122ff3016ea228d43d3216db221f5529
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-08 15:59:50 -0500

    Time: 4 ms (45.57%) | Memory: 19.7 MB (43.07%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        freq = Counter(nums)
-        for (char, count) in freq.items():
-            if count == 1:
-                res = char
-        return res
+        # res = 0
+        # freq = Counter(nums) # O(1) - creating tha map, but internally it runs n times. so, O(n)
+
+        # # Loop unique element times
+        # for (char, count) in freq.items():# gives you all the pairs
+        # # loop for "unique number"
+        #     if count == 1:
+        #         res = char
+        # return res
         
+
+        # # O(n) + O(U) =   O(n)
+
+        res =0
+        for num in nums:
+            res ^= num
+        return res
