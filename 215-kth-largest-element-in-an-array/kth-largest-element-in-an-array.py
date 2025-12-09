9b8410c8bf47d532798032158d04640fa96ea30d
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-09 14:39:58 -0500

    Time: 108 ms (14.61%) | Memory: 28.9 MB (22.92%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,26 +1,26 @@
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k +1
-        minheap = [num for num in nums]
-        heapq.heapify(minheap)
-        res = []
-
-        while len(res)<k:
-            res.append(heapq.heappop(minheap))
-            # print(res)
-        return res[-1]
-
-
-# import heapq
-# from typing import List
-
 # class Solution:
 #     def findKthLargest(self, nums: List[int], k: int) -> int:
-#         minheap = []
+#         k = len(nums) - k +1
+#         minheap = [num for num in nums]
+#         heapq.heapify(minheap)
+#         res = []
 
-#         for num in nums:
-#             heapq.heappush(minheap, num)
-#             if len(minheap) > k:
-#                 heapq.heappop(minheap)    # kick out smallest, keep only k largest
+#         while len(res)<k:
+#             res.append(heapq.heappop(minheap))
+#             # print(res)
+#         return res[-1]
 
-#         return minheap[0]                 # root = kth largest
+
+import heapq
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minheap = []
+
+        for num in nums:
+            heapq.heappush(minheap, num)
+            if len(minheap) > k:
+                heapq.heappop(minheap)    # kick out smallest, keep only k largest
+
+        return minheap[0]                 # root = kth largest
