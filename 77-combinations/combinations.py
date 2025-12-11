1008dd2fc06f7afe5141f445f1a9df3e4e4e83a3
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-11 12:16:07 -0500

    Time: 94 ms (93.70%) | Memory: 59.6 MB (87.61%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,5 +1,23 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # res = []
+        # path = []
+        # def dfs(index):
+        #     if len(path) == k:
+        #         res.append(path.copy())
+        #         return res
+        #     remaining = k - len(path)
+        #     upper_bound = n - remaining + 1
+
+        #     for i in range(index, upper_bound + 1):
+        #         path.append(i)
+        #         dfs(i+1)
+        #         path.pop()
+            
+
+        # dfs(1)
+        # return res
+
         res = []
         path = []
         def dfs(index):
@@ -8,14 +26,14 @@ class Solution:
                 return res
             remaining = k - len(path)
             upper_bound = n - remaining + 1
-
-            for i in range(index, upper_bound + 1):
+            for i in range(index, upper_bound + 1): # This code if for pruning
                 path.append(i)
                 dfs(i+1)
                 path.pop()
-            
-
         dfs(1)
         return res
+            
+
+
                  
         
