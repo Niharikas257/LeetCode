5370fb8faca484060a01f42e9fd8d340f170931c
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-11 11:52:18 -0500

    Time: 98 ms (91.06%) | Memory: 59.7 MB (55.92%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        path = []
+        def dfs(index):
+            if len(path) == k:
+                res.append(path.copy())
+                return res
+            remaining = k - len(path)
+            upper_bound = n - remaining + 1
+
+            for i in range(index, upper_bound + 1):
+                path.append(i)
+                dfs(i+1)
+                path.pop()
+            
+
+        dfs(1)
+        return res
+                 
+        
