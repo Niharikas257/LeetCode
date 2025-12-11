2a83343047c5d6f135b45534fb4bee7e1c0c616e
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-11 12:56:30 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (95.57%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,43 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return []
-        if len(nums) == 1:
-            return [nums]
-        
-        res = []
-        for i in range(len(nums)):
-            cur = nums[i]
-            remain = nums[:i] + nums[i+1:]
-            for p in self.permute(remain):
-                res.append([cur] + p)
-        return res
+        # res = []
+        # path = []
+        # used = [False]*len(nums)
+        # def backtrack():
+        #     if len(path) == len(nums):
+        #         res.append(path.copy())
+        #         return 
 
+        #     for i in range(len(nums)):
+        #         if used[i]:
+        #             continue
+        #         path.append(nums[i])
+        #         used[i] = True
+        #         backtrack()
+        #         path.pop()
+        #         used[i] = False
+        # backtrack()
+        # return res
+
+        res = []
+        path = []
+        visited = set()
+        def dfs():
+            if len(path) == len(nums):
+                res.append(path.copy())
+                return
+            
+            for i in range(len(nums)):
+                if nums[i] in visited:
+                    continue
+                path.append(nums[i])
+                visited.add(nums[i])
+                dfs()
+                path.pop()
+                visited.remove(nums[i])
+        
+        dfs()
+        return res
+            
 
         
