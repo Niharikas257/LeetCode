2b5612761d8df933fec9171cd1ab7243f23a2976
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-12 08:19:20 -0500

    Time: 124 ms (34.54%) | Memory: 33.7 MB (22.17%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,12 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0]*len(temperatures)
-        stack = [] # pair: index, temperature
+        stack = []
+        answer = [0] * len(temperatures)
+        for ind, temp in enumerate(temperatures):
+            while stack and stack[-1][0] < temp:
+                popped_temp, popped_ind = stack.pop()
+                answer[popped_ind] = ind - popped_ind
+            stack.append((temp, ind))
+        return answer
 
-        for i,t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = (i - stackInd)
-            stack.append([t,i])
-        return res
-        
+        # Imagine this like a heavy temperature can compress the lower one. so whenever there is a higher temperature you pop the items before actually pushing the value on the stack. This was, the stack would be monotonically increasing.
+
+
+
+
