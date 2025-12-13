06bfa75fb690af4143a4e6f19eeb341f92b330d3
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-12 22:07:05 -0500

    Time: 2 ms (94.54%) | Memory: 17.7 MB (54.32%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,20 +1,33 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        # last_pos = {}
+        # for i, ch in enumerate(s):
+        #     last_pos[ch] = i # end of the character
+        # start = 0
+        # end = 0 # end of the segment
+        # res = []
+        
+        # for i, ch in enumerate(s):
+        #     end = max(end, last_pos[ch])
+
+        #     if i == end:
+        #         res.append(end-start+1)
+        #         start = i+1
+        # return res
+
+
         last_pos = {}
         for i, ch in enumerate(s):
-            last_pos[ch] = i # end of the character
-        start = 0
-        end = 0 # end of the segment
-        res = []
-        
-        for i, ch in enumerate(s):
-            end = max(end, last_pos[ch])
+            last_pos[ch] = i
 
+        start = 0
+        end = 0
+        res = []
+
+        for i,ch in enumerate(s):
+            end = max(end, last_pos[ch])
             if i == end:
                 res.append(end-start+1)
                 start = i+1
         return res
-
-
-
         
