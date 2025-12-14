497f316742a178b59e08190bf8ee03d00fdc4afc
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-14 10:04:47 -0500

    Time: 0 ms (100.00%) | Memory: 19 MB (84.09%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,23 +1,52 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = [[] for _ in range(numCourses)]
-        indegree = [0]*numCourses
+        # graph = [[] for _ in range(numCourses)]
+        # indegree = [0]*numCourses
 
+        # for course, pre in prerequisites:
+        #     graph[pre].append(course)
+        #     indegree[course] += 1
+
+        # q = deque()
+
+        # for course in range(numCourses):
+        #     if indegree[course] == 0:
+        #         q.append(course)
+        # order = []
+        
+        # while q:
+        #     course = q.popleft()
+        #     order.append(course)
+
+        #     for next_course in graph[course]:
+        #         indegree[next_course] -= 1
+        #         if indegree[next_course] == 0:
+        #             q.append(next_course)
+        
+        # if len(order) == len(indegree):
+        #     return order
+        # return []
+
+#------------------------------------------------------#
+
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
         for course, pre in prerequisites:
             graph[pre].append(course)
-            indegree[course] += 1
+
+        for pre in range(numCourses):
+            for course in graph[pre]:
+                indegree[course] += 1
 
         q = deque()
-
+        
         for course in range(numCourses):
             if indegree[course] == 0:
                 q.append(course)
         order = []
-        
         while q:
             course = q.popleft()
             order.append(course)
-
             for next_course in graph[course]:
                 indegree[next_course] -= 1
                 if indegree[next_course] == 0:
