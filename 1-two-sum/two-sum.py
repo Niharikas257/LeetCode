7204493616b61c3f51588c4589d5963fc76dc0f3
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-21 12:10:07 -0500

    Time: 13 ms (45.57%) | Memory: 14 MB (15.27%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -10,10 +10,22 @@ class Solution(object):
         # 3. Is it absolute difference ? - Yes
         # 4. can the values be nagtive - no - sliding window/ prefix
 
-        seen = {}
-        for i in range(len(nums)):
-            remainder = target - nums[i]
-            if remainder in seen:
-                return [i, seen[remainder]]
-            seen[nums[i]] = i
-        return []
+        # seen = {}
+        # for i in range(len(nums)):
+        #     remainder = target - nums[i]
+        #     if remainder in seen:
+        #         return [i, seen[remainder]]
+        #     seen[nums[i]] = i
+        # return []
+
+        nums_idx = [[num, i] for i, num in enumerate(nums)]
+        nums_idx.sort()
+        left = 0
+        right = len(nums_idx)-1
+        while left < right:
+            if nums_idx[left][0] + nums_idx[right][0] > target:
+                right -= 1
+            elif nums_idx[left][0] + nums_idx[right][0] < target:
+                left += 1
+            else:
+                return [nums_idx[left][1], nums_idx[right][1]]
