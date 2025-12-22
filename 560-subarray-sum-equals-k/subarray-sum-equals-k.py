26be12fd3ed37b2f1282fb95eb0ccd5e0a28ad8d
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-22 13:32:40 -0500

    Time: 33 ms (45.94%) | Memory: 20.2 MB (98.20%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,14 +1,23 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0 # it's the sum of the values in hash table
-        seen = {0:1}
-        prefix_sum = 0
+        # count = 0 # it's the sum of the values in hash table
+        # seen = {0:1}
+        # prefix_sum = 0
 
+        # for num in nums:
+        #     prefix_sum += num
+        #     if (prefix_sum - k) in seen:
+        #         count += seen[prefix_sum-k]
+        #     seen[prefix_sum] = seen.get(prefix_sum, 0) + 1
+        # return count
+        count = 0
+        prefix = 0
+        seen = {0:1}
         for num in nums:
-            prefix_sum += num
-            if (prefix_sum - k) in seen:
-                count += seen[prefix_sum-k]
-            seen[prefix_sum] = seen.get(prefix_sum, 0) + 1
+            prefix += num
+            if prefix-k in seen:
+                count += seen[prefix-k]
+            seen[prefix] = seen.get(prefix, 0) + 1
         return count
 
 
