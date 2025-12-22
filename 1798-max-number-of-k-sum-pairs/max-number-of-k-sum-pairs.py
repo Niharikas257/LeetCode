5c4ec115b3ad6ceafbc47c486114c05de662aaec
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-21 22:34:29 -0500

    Time: 500 ms (69.44%) | Memory: 30 MB (62.17%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,19 +1,37 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        left = 0
-        right = len(nums)-1
-        operations = 0
+        # 1 - When changing the input is allowed.
+        # nums.sort()
+        # left = 0
+        # right = len(nums)-1
+        # operations = 0
 
-        while left < right:
-            if nums[left] + nums[right] < k:
-                left += 1
-            elif nums[left] + nums[right] > k:
-                right -= 1
-            else:
+        # while left < right:
+        #     if nums[left] + nums[right] < k:
+        #         left += 1
+        #     elif nums[left] + nums[right] > k:
+        #         right -= 1
+        #     else:
+        #         operations += 1
+        #         left += 1
+        #         right -= 1
+        # return operations
+
+        # 2 - if we can use extra space, we can use hash.
+        seen = {}
+        operations = 0
+        for i, num in enumerate(nums):
+            remainder = k - num
+            if seen.get(remainder, 0) > 0:
                 operations += 1
-                left += 1
-                right -= 1
+                seen[remainder] -= 1
+            else:
+                seen[num] = seen.get(num,0) + 1
+                
         return operations
+
+
+
+
 
         
