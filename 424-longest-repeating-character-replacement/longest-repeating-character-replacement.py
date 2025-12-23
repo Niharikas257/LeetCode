d828624f05c2a3b15660130741d71ce17c252af1
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-22 21:40:46 -0500

    Time: 116 ms (64.64%) | Memory: 17.8 MB (94.20%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,20 +1,33 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         # left = 0
-        charset = set(s)
+        # right = 0
+        # count = {}
+        # max_freq =  0
+        # res = 0
+
+        # for right in range(len(s)):
+        #     count[s[right]] = count.get(s[right], 0) + 1
+        #     max_freq = max(max_freq, count[s[right]])
+        #     while (right-left+1)  - max_freq > k:
+        #         count[s[left]] -= 1
+        #         left +=1
+        #     res = max(res, right-left+1)
+        # return res
+
+        left = 0
+        right = 0
+        window = {}
+        max_freq = 0
         res = 0
 
-        for c in charset:
-            count = 0
-            left = 0
-            for right in range(len(s)):
-                if s[right] == c:
-                    count += 1
-                while (right-left+1) - count > k:
-                    if s[left] == c:
-                        count -= 1
-                    left += 1
-                res = max(res, (right-left+1))
+        for right in range(len(s)):
+            window[s[right]] = window.get(s[right],0)+1
+            max_freq = max(max_freq, window[s[right]])
+            while (right-left+1) - max_freq > k:
+                window[s[left]] -= 1
+                left += 1
+            res = max(res, right-left+1)
         return res
 
                 
