1e8ae92dc5e58bddb2c98fbc1bb4e21d761811b1
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-23 11:34:22 -0500

    Time: 39 ms (16.91%) | Memory: 18.4 MB (98.91%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def at_most(goal):
+            cur_sum = 0
+            left = 0
+            count = 0
+            for right in range(len(nums)):
+                cur_sum += nums[right]
+                while cur_sum > goal and left <= right:
+                    cur_sum -= nums[left]
+                    left += 1
+                count += right - left + 1
+            return count
+        return at_most(goal) - at_most(goal-1)
+
