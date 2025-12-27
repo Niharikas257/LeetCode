dbd1c8a93d60dacb40bd316b4d4cfcc3469549c6
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-27 10:20:58 -0500

    Time: 7 ms (23.94%) | Memory: 18.8 MB (8.43%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,35 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, val in enumerate(numbers):
-            remainder = target - val
-            if remainder in seen:
-                return[seen[remainder]+1, i+1]
-            seen[val] = i
+
+        # seen = {}
+        # for i, val in enumerate(numbers):
+        #     remainder = target - val
+        #     if remainder in seen:
+        #         return[seen[remainder]+1, i+1]
+        #     seen[val] = i
+
+# 1. we can create a hashmap and use the remainder, if it's in the hashmap then return the index
+
+# 1. we can use 2 pointer technique, because the array is already sorted, so we can add left + right values and if it's bigger than target, we move left, else, we go right
+        # left = 0
+        # right = len(numbers) - 1
+        # while left<right:
+        #     sum = numbers[left] + numbers[right]
+        #     if sum == target:
+        #         return [left+1, right+1]
+        #     elif sum < target:
+        #         left += 1
+        #     else:
+        #         right -= 1
+
+        left =  0
+        right = len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] < target:
+                left += 1
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else: 
+                return [left+1, right+1]        
 
         
