c887d799702ae9dc62ae1c7c4ccc5655850dc97e
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-27 17:56:21 -0500

    Time: 3 ms (95.93%) | Memory: 19.3 MB (80.75%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -7,3 +7,19 @@ class Solution:
         res.sort()
         return res
         
+
+        left = 0
+        right = len(nums)-1
+        res = [0]*n
+        write = len(nums) - 1
+        while left < right:
+            left_square = nums[left] * nums[left]
+            right_square = nums[right] * nums[right]
+            if left_square > right_square:
+                res[write] = left_square
+                left += 1 
+            elif left_square < right_square:
+                res[write] = right_square
+                right -= 1
+            write -= 1
+        return res
