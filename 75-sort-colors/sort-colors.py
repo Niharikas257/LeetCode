143aa4b39fb429c2be207ad45fc64040d927d807
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-28 08:43:46 -0500

    Time: 0 ms (100.00%) | Memory: 17.1 MB (97.84%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,38 +4,54 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # nums.sort()
-        def merge_sort(nums):
-            if len(nums) > 1:
-                mid = len(nums) // 2  # Find the middle of the array
-                left_half = nums[:mid]  # Divide the array into two halves
-                right_half = nums[mid:]
+        # def merge_sort(nums):
+        #     if len(nums) > 1:
+        #         mid = len(nums) // 2  # Find the middle of the array
+        #         left_half = nums[:mid]  # Divide the array into two halves
+        #         right_half = nums[mid:]
 
-                merge_sort(left_half)  # Recursively sort the first half
-                merge_sort(right_half)  # Recursively sort the second half
+        #         merge_sort(left_half)  # Recursively sort the first half
+        #         merge_sort(right_half)  # Recursively sort the second half
 
-                i = j = k = 0
+        #         i = j = k = 0
 
-                # Merge the sorted halves
-                while i < len(left_half) and j < len(right_half):
-                    if left_half[i] < right_half[j]:
-                        nums[k] = left_half[i]
-                        i += 1
-                    else:
-                        nums[k] = right_half[j]
-                        j += 1
-                    k += 1
+        #         # Merge the sorted halves
+        #         while i < len(left_half) and j < len(right_half):
+        #             if left_half[i] < right_half[j]:
+        #                 nums[k] = left_half[i]
+        #                 i += 1
+        #             else:
+        #                 nums[k] = right_half[j]
+        #                 j += 1
+        #             k += 1
 
-                # Copy remaining elements of left_half, if any
-                while i < len(left_half):
-                    nums[k] = left_half[i]
-                    i += 1
-                    k += 1
+        #         # Copy remaining elements of left_half, if any
+        #         while i < len(left_half):
+        #             nums[k] = left_half[i]
+        #             i += 1
+        #             k += 1
 
-                # Copy remaining elements of right_half, if any
-                while j < len(right_half):
-                    nums[k] = right_half[j]
-                    j += 1
-                    k += 1
-            return nums
+        #         # Copy remaining elements of right_half, if any
+        #         while j < len(right_half):
+        #             nums[k] = right_half[j]
+        #             j += 1
+        #             k += 1
+        #     return nums
 
-        merge_sort(nums)
+        # merge_sort(nums)
+
+        # ---------- This is a two pointer pattern where we have to sort the array in-place, and we have certain number of categories to sort them into.----------Pattern D - 2 ended partition. (Because there are 2 pointers which mark the end of low and high). The low pointer will tell you where the next low element will go and high will keep track where high value will go.
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+            else:
+                mid += 1
+        
