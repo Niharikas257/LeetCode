8c3df978a89f18d0276d3d0b4e37a7530ea82cb7
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-28 12:16:56 -0500

    Time: 4 ms (26.75%) | Memory: 17.6 MB (90.60%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,16 +1,32 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1_hash = {}
-        for num in nums1:
-            nums1_hash[num] = nums1_hash.get(num, 0) + 1
+        # nums1_hash = {}
+        # for num in nums1:
+        #     nums1_hash[num] = nums1_hash.get(num, 0) + 1
         
-        nums2_hash = {}
-        for num in nums2:
-            nums2_hash[num] = nums2_hash.get(num, 0) + 1
+        # nums2_hash = {}
+        # for num in nums2:
+        #     nums2_hash[num] = nums2_hash.get(num, 0) + 1
 
-        arr = []
-        for num, c1 in nums1_hash.items():
-            if num in nums2_hash:
-                c2 = nums2_hash[num]
-                arr.extend([num] * min(c1, c2))
-        return arr
+        # arr = []
+        # for num, c1 in nums1_hash.items():
+        #     if num in nums2_hash:
+        #         c2 = nums2_hash[num]
+        #         arr.extend([num] * min(c1, c2))
+        # return arr        
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        j = 0
+        result = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return result
+
