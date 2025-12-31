3cc284312dcc0b3c4c8b452fc860a1e5b7d96215
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2025-12-31 00:21:26 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (92.81%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,8 +1,27 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        maxheap = []
-        for i, num in enumerate(nums):
-            heapq.heappush(maxheap, (-num, i))
-        _, i = heapq.heappop(maxheap)
-        return i
+        # maxheap = []
+        # for i, num in enumerate(nums):
+        #     heapq.heappush(maxheap, (-num, i))
+        # _, i = heapq.heappop(maxheap)
+        # return i
         
+        # left = 0
+        # right = len(nums)-1
+
+        # while left < right:
+        #     mid = (left+right)//2
+        #     if nums[mid] < nums[mid+1]:
+        #         left = mid + 1
+        #     else:
+        #         right = mid
+        # return right
+
+        # stack = []
+        # for i, num in enumerate(nums):
+        #     while stack and stack[-1]
+        a = max(nums)
+        for i,num in enumerate(nums):
+            if num == a:
+                return i
+
