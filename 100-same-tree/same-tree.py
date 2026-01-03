2a7dcc94f2abf95a5fcdf08a8f29a0d9c8663759
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-03 00:09:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.06%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,21 +6,43 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # if p is None and q is None:
-        #     return True
-        # if p and q and p.val == q.val:
-        #     return self.isSameTree(q.left, p.left) and self.isSameTree(q.right, p.right)
-        # else:
-        #     return False
         if p is None and q is None:
             return True
         if p is None or q is None:
             return False
         
+        # def bfs(node):
+        #     res = []
+        #     q = collections.deque()
+        #     q.append(node)
+        #     while q:
+        #         cur = q.popleft()
+        #         if cur is None:
+        #             res.append(None)
+        #             continue
+        #         res.append(cur.val)
+        #         if cur.left:
+        #             q.append(cur.left)
+        #         if cur.left is None:
+        #             q.append(None)
+        #         if cur.right:
+        #             q.append(cur.right)
+        #         if cur.right is None:
+        #             q.append(None)
+                
+        #     return res
+        
+        # res_p = bfs(p)
+        # res_q = bfs(q)
+        # if res_p and res_q and res_p == res_q:
+        #     return True
+        # else: 
+        #     return False
+        
         def bfs(node):
-            res = []
-            q = collections.deque()
+            q = deque()
             q.append(node)
+            res = []
             while q:
                 cur = q.popleft()
                 if cur is None:
@@ -35,12 +57,10 @@ class Solution:
                     q.append(cur.right)
                 if cur.right is None:
                     q.append(None)
-                
             return res
-        
         res_p = bfs(p)
         res_q = bfs(q)
         if res_p and res_q and res_p == res_q:
             return True
-        else: 
+        else:
             return False
