9729d358b901c8d2e1a43fafa5ba62ed54029ba8
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-03 00:00:19 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (87.09%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,15 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # 1. dfs
-        # 2. recursion
-        # 3. stack - iterative
-        # 4. define a function and keep calling dfs(node.left) and dfs(node.right)
-        # 5. each step we find the absolute difference of left and right
-        #     if more than 1 - false
-        #     else true
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:            
+        # def dfs(node):
+        #     if node is None:
+        #         return True
+        #     left_height = dfs(node.left)
+        #     if left_height == -1:
+        #         return -1
+        #     right_height = dfs(node.right)
+        #     if right_height == -1:
+        #         return -1
             
+        #     if abs(right_height - left_height) >1:
+        #         return -1
+
+        #     return max(left_height, right_height) + 1
+        
+        # return dfs(root) != -1
+
+
         def dfs(node):
             if node is None:
                 return True
@@ -23,11 +33,9 @@ class Solution:
             right_height = dfs(node.right)
             if right_height == -1:
                 return -1
-            
-            if abs(right_height - left_height) >1:
+            if abs(right_height - left_height) > 1:
                 return -1
-
-            return max(left_height, right_height) + 1
-        
+            return 1 + max(left_height, right_height)
         return dfs(root) != -1
+
         
