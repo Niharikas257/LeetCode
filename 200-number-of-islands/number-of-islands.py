2014b1d2e65b809b19f466131a91537deb42c554
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-06 08:41:37 -0500

    Time: 238 ms (71.34%) | Memory: 22.3 MB (33.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,59 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # rows = len(grid)
-        # cols = len(grid[0])
-        # dir = [(1,0), (-1,0), (0,1), (0,-1)]
-        # visited = [[False]*cols for _ in range(rows)]
-        
-
-        # def dfs(row, col):
-        #     if row<0 or col<0 or row>= rows or col>= cols:
-        #         return
-        #     if visited[row][col]:
-        #         return
-        #     if grid[row][col] == "0":
-        #         return
-        #     visited[row][col] = True
-        #     for dr, dc in dir:
-        #         dfs(row+dr, col+dc)
-        # islands = 0 
-        # for row in range(rows):
-        #     for col in range(cols):
-        #         if not visited[row][col] and grid[row][col] == "1":
-        #             islands += 1
-        #             dfs(row, col)
-        # return islands
-
-
         rows = len(grid)
-        cols =len(grid[0])
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        visited = set()
-
-        def dfs(row, col):
-            if row < 0 or col < 0 or row >= rows or col >= cols:
+        cols = len(grid[0])
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        visited = [[False] * cols for _ in range(rows)]
+        def dfs(r,c):
+            if r< 0 or c < 0 or r >= rows or c >= cols:
                 return
-            if (row,col) in visited:
+            if grid[r][c] == "0":
                 return
-            if grid[row][col] == "0":
+            if visited[r][c]:
                 return
-            visited.add((row, col))
-            for dr,dc in directions:
-                nr = row + dr
-                nc = col + dc
+            visited[r][c] = True # Make it visited
+            for (dr, dc) in directions:
+                nr = dr + r
+                nc = dc + c
                 dfs(nr, nc)
-            return 1
+            
         
         islands = 0
-
         for row in range(rows):
             for col in range(cols):
-                if (row,col) not in visited and grid[row][col] == "1":
-                    islands += dfs(row, col)
-                    
+                if grid[row][col] == "1" and not visited[row][col]:
+                    dfs(row, col)
+                    islands += 1
         return islands
 
-
-
-
-        
