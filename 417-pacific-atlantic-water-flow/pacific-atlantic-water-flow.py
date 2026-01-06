ffe2febfbcf337f57a956d6249300f695e2e82ed
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-06 11:34:57 -0500

    Time: 23 ms (93.52%) | Memory: 22.2 MB (5.18%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,47 +1,75 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights or not heights[0]:
-            return[]
+        # if not heights or not heights[0]:
+        #     return[]
+        # rows = len(heights)
+        # cols = len(heights[0])
+        # directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        # pacific = [[False]*cols for _ in range(rows)]
+        # atlantic = [[False]*cols for _ in range(rows)]
+
+        # def dfs(row, col, visited):
+        #     if row < 0 or col <0 or row>= rows or col >= cols:
+        #         return
+        #     if visited[row][col]:
+        #         return
+        #     visited[row][col] = True
+        #     for (dr, dc) in directions:
+        #         nr = row + dr
+        #         nc = col + dc
+
+        #         if nr < 0 or nc <0 or nr>= rows or nc >= cols:
+        #             continue
+        #         if visited[nr][nc] == True:
+        #             continue
+        #         if heights[nr][nc] < heights[row][col]:
+        #             continue
+        #         dfs(nr, nc, visited)
+        # result = []
+        # for c in range(cols):
+        #     dfs(0, c, pacific)
+        #     dfs(rows-1, c, atlantic)
+        # for r in range(rows):
+        #     dfs(r, 0, pacific)
+        #     dfs(r, cols-1, atlantic)
+        # for row in range(rows):
+        #     for col in range(cols):
+        #         if pacific[row][col] and atlantic[row][col]:
+        #             result.append([row, col])
+        #             # print(result)
+        # return result
+
         rows = len(heights)
         cols = len(heights[0])
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        pacific = [[False]*cols for _ in range(rows)]
-        atlantic = [[False]*cols for _ in range(rows)]
-
-        # q = deque()
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        pacific = [[False]* cols for _ in range(rows)]
+        atlantic = [[False] * cols for _ in range(rows)]
 
         def dfs(row, col, visited):
+            if row < 0 or col < 0 or row >= rows or col>= cols:
+                return
+            if visited[row][col]:
+                return
             visited[row][col] = True
             for (dr, dc) in directions:
-                new_row = row + dr
-                new_col = col + dc
-
-                if new_row<0 or new_col <0 or new_col >= cols or new_row>=rows:
+                nr = dr + row
+                nc = dc + col
+                if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
                     continue
-                if visited[new_row][new_col] == True:
+                if visited[nr][nc] :
                     continue
-
-                if heights[new_row][new_col] < heights[row][col]:
+                if heights[nr][nc] < heights[row][col]:
                     continue
-                
-                dfs(new_row, new_col, visited)
-                
-
-        # pacific = []
-        # atlantic = []
+                dfs(nr, nc, visited)
         result = []
-        
         for c in range(cols):
-            dfs(0, c, pacific)          # top edge
-            dfs(rows - 1, c, atlantic)  # bottom edge
-
+            dfs(0, c, pacific)
+            dfs(rows-1, c, atlantic)
         for r in range(rows):
-            dfs(r, 0, pacific)          # left edge
-            dfs(r, cols - 1, atlantic)
-
+            dfs(r, 0, pacific)
+            dfs(r, cols-1, atlantic)
         for row in range(rows):
             for col in range(cols):
                 if pacific[row][col] and atlantic[row][col]:
                     result.append([row, col])
-
         return result
