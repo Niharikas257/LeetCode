9b147ce8a524d019bff0a605edcb4bd356b6ae49
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-06 17:16:52 -0500

    Time: 4 ms (64.27%) | Memory: 20.4 MB (5.37%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,15 +1,38 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        for course,pre in prerequisites:
-            graph[pre].append(course)
+        # graph = [[] for _ in range(numCourses)]
+        # for course,pre in prerequisites:
+        #     graph[pre].append(course)
             
-        indegree = [0] * numCourses
+        # indegree = [0] * numCourses
+        # for pre in range(numCourses):
+        #     for course in graph[pre]:
+        #         indegree[course] += 1
+
+
+        # q = deque()
+        # for course in range(numCourses):
+        #     if indegree[course] == 0:
+        #         q.append(course)
+        # order = []
+        # while q:
+        #     course = q.popleft()
+        #     order.append(course)
+        #     for next_course in graph[course]:
+        #         indegree[next_course]-=1
+        #         if indegree[next_course] == 0:
+        #             q.append(next_course)
+        # return True if len(order) == len(indegree) else False
+
+
+        graph = [[] for _ in range(numCourses)]
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+        
+        indegree = [0]*numCourses
         for pre in range(numCourses):
             for course in graph[pre]:
-                indegree[course] += 1
-
-
+                indegree[course] +=1 
         q = deque()
         for course in range(numCourses):
             if indegree[course] == 0:
@@ -19,7 +42,7 @@ class Solution:
             course = q.popleft()
             order.append(course)
             for next_course in graph[course]:
-                indegree[next_course]-=1
+                indegree[next_course] -=1
                 if indegree[next_course] == 0:
                     q.append(next_course)
         return True if len(order) == len(indegree) else False
