9a2f0903aa2b6e08761d843071efb6b0a565921f
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-08 14:17:31 -0500

    Time: 106 ms (16.76%) | Memory: 31.3 MB (8.42%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,9 +1,18 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # minheap = []
+        # for num in nums:
+        #     heapq.heappush(minheap, num)
+        #     if len(minheap) > k:
+        #         heapq.heappop(minheap) # When you pop from the heap if the len becomes more than k means there are just k elements in the heap, and because this is a minheap, so the popped value is the smallest value which we anyways don't need. So, heap has k values which and does not have values which we don't need. In other words, heap has k larger values because we are popping the smaller values. So, at any point when you add a value, the smaller one goes to the top and if the size of the heap exceeds k that means heap has more than k values so we need to pop, and we pop the value which we don't need which is the smaller value. So, heap top has smallest of larger values.
+        # return minheap[0]
+
         minheap = []
         for num in nums:
             heapq.heappush(minheap, num)
-            if len(minheap) > k:
-                heapq.heappop(minheap) # When you pop from the heap if the len becomes more than k means there are just k elements in the heap, and because this is a minheap, so the popped value is the smallest value which we anyways don't need. So, heap has k values which and does not have values which we don't need. In other words, heap has k larger values because we are popping the smaller values. So, at any point when you add a value, the smaller one goes to the top and if the size of the heap exceeds k that means heap has more than k values so we need to pop, and we pop the value which we don't need which is the smaller value. So, heap top has smallest of larger values.
+        
+        while len(minheap) > k:
+            heapq.heappop(minheap)
+        
         return minheap[0]
 
