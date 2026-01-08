a97f25c46b3e6f6feeb87f9d96d22330bec6c588
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-08 14:25:37 -0500

    Time: 107 ms (15.97%) | Memory: 30.6 MB (8.95%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -7,12 +7,21 @@ class Solution:
         #         heapq.heappop(minheap) # When you pop from the heap if the len becomes more than k means there are just k elements in the heap, and because this is a minheap, so the popped value is the smallest value which we anyways don't need. So, heap has k values which and does not have values which we don't need. In other words, heap has k larger values because we are popping the smaller values. So, at any point when you add a value, the smaller one goes to the top and if the size of the heap exceeds k that means heap has more than k values so we need to pop, and we pop the value which we don't need which is the smaller value. So, heap top has smallest of larger values.
         # return minheap[0]
 
-        minheap = []
+        # minheap = []
+        # for num in nums:
+        #     heapq.heappush(minheap, num)
+        
+        # while len(minheap) > k:
+        #     heapq.heappop(minheap)
+        
+        # return minheap[0]
+
+
+        maxheap = []
         for num in nums:
-            heapq.heappush(minheap, num)
-        
-        while len(minheap) > k:
-            heapq.heappop(minheap)
-        
-        return minheap[0]
+            heapq.heappush(maxheap, -num)
+        while len(maxheap) > len(nums)-k+1:
+            heapq.heappop(maxheap)
+        return -maxheap[0]
+
 
