ba8d4e221a72dac74c42e3204f1ff06bb9792138
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-08 16:26:00 -0500

    Time: 7 ms (95.77%) | Memory: 25.4 MB (5.31%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,20 +1,17 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.minHeap = nums
+        self.minheap = nums
         self.k = k
-        # Now this nums/ self.minHeap is just an array, we need to heapify it so this array will have the properties of the min heap
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap)> self.k:
-            heapq.heappop(self.minHeap)
+        heapq.heapify(self.minheap)
+        # while len(self.minheap) > k:
+        #     heapq.heappop(self.minheap)
+        # You need to trim the heap size in __init__ because if the size of minheap before first add is bigger than k, let's say m, then add will only pop the element to make it m-1, not k.     
+    def add(self, val):
+        heapq.heappush(self.minheap, val)
+        while len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+        return self.minheap[0]
+    
         
-    def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap, val) # This will push the value in the min heap, maintaining the heap invariant.
-        if len(self.minHeap)> self.k:
-            heapq.heappop(self.minHeap)
-        return self.minHeap[0]
 
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
