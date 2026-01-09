b94370e797fb1ffa9baa3c0983fc25dfb05d9702
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-09 10:26:54 -0500

    Time: 266 ms (76.22%) | Memory: 47.7 MB (5.25%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,26 +1,24 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        # 1. You are using maxheap to make choice when the capital is enough for multiple projects, in that case, you will choose the project with maximum profit.
         n = len(profits)
-        projects = [(capital[i], profits[i]) for i in range(n)]
-        projects.sort(key = lambda x:x[0])
-
-        maxheap = []
-
-        idx = 0
-
+        index = 0
         current_capital = w
-
-        for _ in range(k):
-            while idx < n and projects[idx][0] <= current_capital:
-                required_capital, profit = projects[idx]
-                heapq.heappush(maxheap, -profit)
-                idx += 1
+        best_profit = 0
+        maxheap = []
+        projects = [(capital[i], profits[i]) for i in range(n)]
+        projects.sort(key=lambda x:x[0]) # we will start from the one which needs minimum capital
+        for _ in range(k): # Run the loop k times, we don't require the variable that's why _
+            while index < len(projects) and current_capital >= projects[index][0]:
+                required_capital, profit = projects[index]
+                heapq.heappush(maxheap, -profit) # If we have enough capital to start a project, we will pick the one with maximum profit.
+                index +=1 
             
             if not maxheap:
                 break
             
-            best_profit = -1 * heapq.heappop(maxheap)
-
+            best_profit = -1*heapq.heappop(maxheap)
             current_capital += best_profit
-
+        
         return current_capital
+
