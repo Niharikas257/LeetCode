2b8d544bdd9c38fa04e83467b24718c0c73e7c9e
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-09 10:53:27 -0500

    Time: 65 ms (97.22%) | Memory: 39.6 MB (15.22%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -1,19 +1,39 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        minheap = [(nums1[0] + nums2[0], 0, 0)] # -> sum of the first 2 values from both the arrays, and indices
-        visited = set((0,0))
-        res = []
+        # minheap = [(nums1[0] + nums2[0], 0, 0)] # -> sum of the first 2 values from both the arrays, and indices
+        # visited = set((0,0))
+        # res = []
 
-        while minheap and len(res)<k:
+        # while minheap and len(res)<k:
+        #     _, i, j = heapq.heappop(minheap)
+        #     res.append((nums1[i],nums2[j]))
+        #     if i+1 < len(nums1) and (i+1, j) not in visited:
+        #         visited.add((i+1, j))
+        #         heapq.heappush(minheap, ((nums1[i+1] + nums2[j]), i+1, j))
+        #     if j+1 < len(nums2) and (i, j+1) not in visited:
+        #         visited.add((i, j+1))
+        #         heapq.heappush(minheap, ((nums1[i] + nums2[j+1]), i, j+1))
+            
+        # return res
+            
+
+
+        minheap = [(nums1[0] + nums2[0], 0, 0)] # Sorted based on the sum
+        i = 0
+        j = 0
+        result = []
+        visited = set((0,0))
+        while minheap and len(result) < k:
             _, i, j = heapq.heappop(minheap)
-            res.append((nums1[i],nums2[j]))
+            result.append((nums1[i], nums2[j]))
             if i+1 < len(nums1) and (i+1, j) not in visited:
+                heapq.heappush(minheap, ((nums1[i+1] + nums2[j]), i+1,j))
                 visited.add((i+1, j))
-                heapq.heappush(minheap, ((nums1[i+1] + nums2[j]), i+1, j))
             if j+1 < len(nums2) and (i, j+1) not in visited:
                 visited.add((i, j+1))
-                heapq.heappush(minheap, ((nums1[i] + nums2[j+1]), i, j+1))
-            
-        return res
-            
+                heapq.heappush(minheap, ((nums1[i]+nums2[j+1]), i, j+1))
+        return result
+
+
+
         
