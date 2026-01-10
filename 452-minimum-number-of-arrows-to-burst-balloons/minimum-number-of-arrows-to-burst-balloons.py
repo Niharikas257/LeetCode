a0d51a10246ae67ae14b2b51294e7ca00791363b
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-10 14:25:45 -0500

    Time: 80 ms (59.55%) | Memory: 53.6 MB (5.14%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,21 +1,17 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        # if points is None:
-        #     return 0
-        points.sort(key = lambda x:x[1]) # sort the array based on the end value, 0 indexed.
 
-        arrow_count = 1 # we always need one arrow unless the points is empty
-        arrow_pos = points[0][1] # first arrow at the end of the first balloon
-
-        for new_start, new_end in points:
-            if new_start<=arrow_pos and arrow_pos <= new_end:
-                continue # Because the balloons overlap so we do not need one more arrow
-            else:
-                arrow_count += 1
-                arrow_pos = new_end
+        if not points:
+            return 0
         
-        return arrow_count
+        points.sort(key=lambda x:x[1]) # Greedy choice, sort by end
+        arrows = 1
+        shot = points[0][1]
 
 
-
-
+        for start, end in points[1:]:
+            if start<=shot:
+                continue
+            arrows += 1
+            shot = end
+        return arrows        
