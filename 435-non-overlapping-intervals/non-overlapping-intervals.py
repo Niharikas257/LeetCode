48eb278a9c11089d2eaae2f6cd4a8cdbcf1cf4c4
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-10 13:28:19 -0500

    Time: 84 ms (56.94%) | Memory: 49.4 MB (62.62%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,22 +1,5 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # intervals.sort(key = lambda x:x[1])
-        # overlap = 0
-        # # no_overlap = 1 # because you can always pick one interval as an answer even if all the intervals overlap.
-        # old_end = intervals[0][1]
-
-        # for i in range(1, len(intervals)):
-        #     new_start, new_end = intervals[i]
-
-        #     if old_end > new_start:
-        #         overlap += 1
-        #     else:
-        #         # new_start >= old_end
-        #         # no_overlap += 1
-        #         old_end = new_end
-        
-        # return overlap
-
         if not intervals:
             return 0
         intervals.sort(key=lambda x:x[1]) # sort by end time so we have more scope to add intervals
@@ -26,7 +9,7 @@ class Solution:
         current_end = intervals[0][1]
 
         for new_start, new_end in intervals[1:]:
-            if new_start < current_end:
+            if new_start < current_end: # Usually closed brackets are <= (overlapping) but it is mentioned that they are not, so we should consider them as open bracket.
                 removals += 1
             else:
                  current_end = new_end
