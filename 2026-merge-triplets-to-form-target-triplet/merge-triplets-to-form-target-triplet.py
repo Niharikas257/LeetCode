1dc431a1fff3f621551776fe955a66a2d478e351
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-10 10:02:47 -0500

    Time: 15 ms (72.55%) | Memory: 54 MB (64.09%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -2,19 +2,35 @@ class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
 
 
+        # x,y,z = target
+
+        # have_a = have_b = have_c = False    
+
+        # for a,b,c in triplets:
+        #     if a > x or b > y or c > z:
+        #         continue
+            
+        #     if a == x:
+        #         have_a = True
+        #     if b == y:
+        #         have_b = True
+        #     if c == z:
+        #         have_c = True
+        # return have_a and have_b and have_c
+
         x,y,z = target
 
         have_a = have_b = have_c = False    
 
-        for a,b,c in triplets:
-            if a > x or b > y or c > z:
+        for triplet in triplets:
+            if triplet[0]> x or triplet[1] > y or triplet[2] > z:
                 continue
             
-            if a == x:
+            if triplet[0] == x:
                 have_a = True
-            if b == y:
+            if triplet[1] == y:
                 have_b = True
-            if c == z:
+            if triplet[2] == z:
                 have_c = True
         return have_a and have_b and have_c
 
