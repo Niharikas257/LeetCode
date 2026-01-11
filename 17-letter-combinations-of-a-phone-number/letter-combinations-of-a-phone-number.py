55474ed29b529899bbce7f89b7cd4cfe34349aa5
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-10 23:39:42 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,52 +1,40 @@
-# class Solution:
-#     def letterCombinations(self, digits: str) -> List[str]:
-#         res = []
-#         digitToChar = {
-#             "2": "abc",
-#             "3": "def",
-#             "4": "ghi",
-#             "5": "jkl",
-#             "6": "mno",
-#             "7": "qprs",
-#             "8": "tuv",
-#             "9": "wxyz",
-#         }
-#         def dfs(i, subset): 
-#             # i will track the digits
-#             if len(subset) == len(digits):
-#                 res.append(subset)
-#                 return
-#             for c in digitToChar[digits[i]]:
-#                 dfs(i+1, subset + c)
-#         if digits:
-#             dfs(0,"")
-#         return res
-        
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
-        if not digits:
-            return
-        digitToChar = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "qprs",
-            "8": "tuv",
-            "9": "wxyz",
-        }
-        def backtrack(i, path):
-            if i == len(digits):
-                res.append("".join(path))
-                return
+        digits_to_char = {  "2":"abc",
+                            "3":"def",
+                            "4":"ghi",
+                            "5":"jkl",
+                            "6":"mno",
+                            "7":"pqrs",
+                            "8":"tuv",
+                            "9":"wxyz" }
 
-            possibleLetters = digitToChar[digits[i]]
-            for letter in possibleLetters:
-                path.append(letter)
-                backtrack(i+1, path)
+        # res = []
+        # path = []
+
+        # def dfs(index):
+        #     if index == len(digits):
+        #         res.append("".join(path.copy()))
+        #         return
+        #     for ch in digits_to_char[digits[index]]:
+        #         path.append(ch)
+        #         dfs(index+1)
+        #         path.pop()
+        # dfs(0)
+        # return res
+        
+        res = []
+        path = []
+
+        def dfs(index):
+            if index == len(digits):
+                res.append("".join(path.copy()))
+                return
+            for ch in digits_to_char[digits[index]]:
+                path.append(ch)
+                dfs(index+1)
                 path.pop()
-        backtrack(0,[])
+        dfs(0)
         return res
 
+ 
