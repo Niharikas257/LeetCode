bc32177bd39f4cbd91dadb3aea650ea73ca51808
Niharika Sharma <158223705+Niharikas257@users.noreply.github.com>
2026-01-10 22:15:43 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.32%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,70 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # res = []
-        # subset = []
-        # nums.sort()
-        # seen =[]
-        # def backtrack(i):
-        #     if i >= len(nums):
-        #         t = tuple(subset)
-        #         if t not in seen:
-
-        #             res.append(subset.copy())
-        #             seen.append(t)
-        #         return
-            
-        #     subset.append(nums[i])
-        #     backtrack(i+1)
-        #     subset.pop()
-        #     backtrack(i+1)    
-            
-        # backtrack(0)
-        # return res
-
-
-
-
-        # res = []
-        # subset = []
-        # nums.sort()
-        # def backtrack(start):
-        #     res.append(subset.copy())
-        #     for i in range(start, len(nums)):
-        #         if i > start and nums[i] == nums[i-1]:
-        #             continue
-        #         subset.append(nums[i])
-        #         backtrack(i+1)
-        #         subset.pop()
-        # backtrack(0)
-        # return res
-        
-
-        res = []
-        subset = []
-        nums.sort()
-        def backtrack(start):
-            res.append(subset.copy())
-            for i in range(start, len(nums)):
-                if i > start and nums[i] == nums[i-1]:
+        nums.sort() # IMP
+        res =[]
+        path = []
+        def dfs(index, path):
+            res.append(path.copy())
+            for i in range(index, len(nums)):
+                if i > index and nums[i] == nums[i-1]:
                     continue
-                subset.append(nums[i])
-                backtrack(i+1)
-                subset.pop()
-        backtrack(0)
+                path.append(nums[i])
+                dfs(i+1, path)
+                path.pop()
+
+        dfs(0,[])
         return res
 
-        res = []
-        subset = []
-        visited = []
-        def dfs(i):
-            if i >= len(nums):
-                t = tuple[subset]
-                if t not in seen:
-                    res.append(subset.copy())
-                    return
-            subset.append(nums[i])
-            backtrack(i+1)
-            subset.pop()
-            backtrack(i+1)
-        backtrack(0)
-        return res
+            
